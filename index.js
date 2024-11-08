b32843dc9f2f42c97bc7d2b7c89bc17d147cbc59
Алексей Кесслер <aleksejkessler@MacBook-Air-Aleksej.local>
2024-11-08 17:43:21 +0300

    fix: amvera.yml

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const { createCanvas, loadImage } = require("canvas");
 
 const app = express();
-const port = 3000;
+const port = 80;
 
 app.get("/certificate", async (req, res) => {
   try {
